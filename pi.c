a69235846feb07f361efe6c62458d5b8af285d9e
root <root@pedro-notebook>
2024-09-20 16:08:09 -0300

    a

--- pi.c
+++ pi.c
@@ -22,7 +22,10 @@ int main()
     mpf_set_default_prec(1024); // Precisão padrão - 1024
 
     mpf_t pi_aprox_cima, pi_anterior_cima, erro_aproximado_cima, erro_exato_cima, numerador, denominador, termo, mpf_tolerancia;
+    mpf_t pi_aprox_baixo, pi_anterior_baixo, erro_aproximado_baixo, erro_exato_baixo;
+
     mpf_inits(pi_aprox_cima, pi_anterior_cima, erro_aproximado_cima, erro_exato_cima, numerador, denominador, termo, mpf_tolerancia, NULL);
+    mpf_inits(pi_aprox_baixo, pi_anterior_baixo, erro_aproximado_baixo, erro_exato_baixo, NULL);
 
     printf("Digite a tolerância: ");
     scanf("%lf", &tolerancia);
@@ -46,7 +49,7 @@ int main()
         mpf_init(fatorial_n);
         fatorial(fatorial_n, n);
 
-        // 2^n*k!*k!
+        // 2^n*n!*n!
         mpf_mul(numerador, numerador, fatorial_n);
         mpf_mul(numerador, numerador, fatorial_n);
 
@@ -65,11 +68,11 @@ int main()
 
         n++;
 
-        gmp_printf("------Iteração: %d\n", n - 1);
-        gmp_printf("Numerador: %.Ff\n", numerador);
-        gmp_printf("Denominador: %.Ff\n", denominador);
-        gmp_printf("Pi aprox: %.15Fe\n", pi_aprox_cima);
-        gmp_printf("Erro relativo: %.15Fe\n\n", erro_aproximado_cima);
+        // gmp_printf("------Iteração: %d\n", n - 1);
+        // gmp_printf("Numerador: %.Ff\n", numerador);
+        // gmp_printf("Denominador: %.Ff\n", denominador);
+        // gmp_printf("Pi aprox: %.15Fe\n", pi_aprox_cima);
+        // gmp_printf("Erro relativo: %.15Fe\n\n", erro_aproximado_cima);
 
         mpf_clear(fatorial_n);
     } 
@@ -80,20 +83,75 @@ int main()
     mpf_sub(erro_exato_cima, erro_exato_cima, pi_aprox_cima);
     mpf_abs(erro_exato_cima, erro_exato_cima);
 
+    // Arredondamento para baixo
+    fesetround(FE_DOWNWARD);
+    n = 0;
+    do 
+    {
+        mpf_set(pi_anterior_baixo, pi_aprox_baixo);
+
+        // Numerador
+        // 2^n
+        mpf_set_ui(numerador, 1);
+        for (int i = 0; i < n; i++)
+            mpf_mul_ui(numerador, numerador, 2);
+
+        // n!
+        mpf_t fatorial_n;
+        mpf_init(fatorial_n);
+        fatorial(fatorial_n, n);
+
+        // 2^n*n!*n!
+        mpf_mul(numerador, numerador, fatorial_n);
+        mpf_mul(numerador, numerador, fatorial_n);
+
+        // Denominador - (2n + 1)!
+        fatorial(denominador, 2 * n + 1);
+
+        // Numerador / Denominador
+        mpf_div(termo, numerador, denominador);
+
+        // Soma o termo obtido no somatório na aprox total
+        mpf_add(pi_aprox_baixo, pi_aprox_baixo, termo);
+
+        // Erro aproximado de N com N - 1
+        mpf_sub(erro_aproximado_baixo, pi_aprox_baixo, pi_anterior_baixo);
+        mpf_abs(erro_aproximado_baixo, erro_aproximado_baixo);
+
+        n++;
+
+        // gmp_printf("------Iteração: %d\n", n - 1);
+        // gmp_printf("Numerador: %.Ff\n", numerador);
+        // gmp_printf("Denominador: %.Ff\n", denominador);
+        // gmp_printf("Pi aprox: %.15Fe\n", pi_aprox_baixo);
+        // gmp_printf("Erro relativo: %.15Fe\n\n", erro_aproximado_baixo);
+
+        mpf_clear(fatorial_n);
+    } 
+    while (mpf_cmp(erro_aproximado_baixo, mpf_tolerancia) > 0);
+
+    mpf_mul_ui(pi_aprox_baixo, pi_aprox_baixo, 2); // Porque a fórmula é pi/2
+    mpf_set_d(erro_exato_baixo, M_PI);
+    mpf_sub(erro_exato_baixo, erro_exato_baixo, pi_aprox_baixo);
+    mpf_abs(erro_exato_baixo, erro_exato_baixo);
+
     // mpf -> double
     double pi_aprox_cima_d = mpf_get_d(pi_aprox_cima);
     double erro_aproximado_cima_d = mpf_get_d(erro_aproximado_cima);
     double erro_exato_cima_d = mpf_get_d(erro_exato_cima);
+    double pi_aprox_baixo_d = mpf_get_d(pi_aprox_baixo);
 
     // double -> unsigned long long int
     unsigned long long int pi_aprox_cima_hex = *(unsigned long long int*)&pi_aprox_cima_d;
     unsigned long long int erro_aproximado_cima_hex = *(unsigned long long int*)&erro_aproximado_cima_d;
     unsigned long long int erro_exato_cima_hex = *(unsigned long long int*)&erro_exato_cima_d;
+    unsigned long long int pi_aprox_baixo_hex = *(unsigned long long int*)&pi_aprox_baixo_d;
 
     printf("%d\n", n);
     printf("%.15e %llX\n", erro_aproximado_cima_d, erro_aproximado_cima_hex);
     printf("%.15e %llX\n", erro_exato_cima_d, erro_exato_cima_hex);
     printf("%.15e %llX\n", pi_aprox_cima_d, pi_aprox_cima_hex);
+    printf("%.15e %llX\n", pi_aprox_baixo_d, pi_aprox_baixo_hex);
 
     mpf_clears(pi_aprox_cima, pi_anterior_cima, erro_aproximado_cima, erro_exato_cima, numerador, denominador, termo, mpf_tolerancia, NULL);
 
